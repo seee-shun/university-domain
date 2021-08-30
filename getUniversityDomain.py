a93f13f7a31144a3ae30f90ca950e38b674c0f73
seee-shun <seino.shungo.17@shizuoka.ac.jp>
2021-08-30 11:33:12 +0900

    refactoring

--- getUniversityDomain.py
+++ getUniversityDomain.py
@@ -2,15 +2,16 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import json
 
 html_doc = requests.get("https://so-zou.jp/software/tech/network/domain/ac-jp.htm").text
-soup = BeautifulSoup(html_doc, 'html.parser') # BeautifulSoupの初期化
+soup = BeautifulSoup(html_doc, 'lxml') # BeautifulSoupの初期化 html.parserより処理が高速
 # print(soup.prettify())
 
-university_domain = []
-name = soup.find_all('td',text=re.compile('大学'))
+name = soup.find_all('td',text=re.compile(r'大学|大學'))
 area = ''
-domain = '.ac.jp'
+university_domain = []
+
 
 address = soup.find_all('strong')
 for i,n in enumerate(name):
@@ -80,37 +81,10 @@ for i,n in enumerate(name):
         area = '四国'
     elif 729 <= i <= 785:
         area = '九州'
-    
-    jp_pattern = re.compile(r'奈良先進科学技術大学大学院|北海学園大学|弘前医療福祉大学|尚絅学院大学|仙台大学|東北文教大学|日本ウェルネススポーツ大学|白鴎大学|聖学院大学|日本薬科大学|聖徳大学|神奈川工科大学|横浜薬科大学|高千穂大学|玉川大学|玉川大学|東洋学園大学|日本教育大学院大学|早稲田大学|身延山大学|清泉女学院大学|岐阜女子大学|名古屋学院大学|びわこ成蹊スポーツ大学|京都医療科学大学|立命館大学|大阪成蹊大学|大阪総合保育大学|奈良学園大学|吉備国際大学|広島経済大学|宇部フロンティア大学|沖縄科学技術大学院大学')
-    lg_jp_pattern = re.compile(r'千葉県立保健医療大学')
-    net_pattern = re.compile(r'富士大学|愛知産業大学')
-    com_pattern = re.compile(r'聖カタリナ大学')
-    ac_pattern = re.compile(r'ビジネス・ブレークスルー大学')
 
-    if bool(jp_pattern.search(n)):
-        domain = '.jp'
-    elif bool(lg_jp_pattern.search(n)):
-        domain = '.lg.jp'
-    elif bool(net_pattern.search(n))
-        domain = '.net'
-    elif bool(com_pattern.search(n)):
-        domain = '.com'
-    elif bool(ac_pattern.search(n)):
-        domain = '.ac'
-
-    university_domain.append({'name': n.string,'address':'@' + address[i].string + domain,'area':area,'school_color':''})
+    university_domain.append({'name': n.string,'address':address[i].string,'area':area,'school_color':''})
     
 
 # print(university_domain) 
-# TODO2 このページのaタグをすべて抜き出してください。(HTMLの内容)
-
-# for tag in real_page_tags:
-#     print(tag.string)
-
-# elems = soup('#g-contents > table:nth-of-type(6) > tbody > tr:nth-of-type(2) > td:nth-of-type(1)')
-# print(elems)
-# TODO3 このページのaタグをすべて抜き出してください。(HTMLの内容)
-
-# for tag in real_page_tags:
-#     print(tag.get("href")) 
-#g-contents > table:nth-child(6) > tbody > tr:nth-child(2) > td:nth-child(1)
+with open('./university.json', 'w') as f:
+    json.dump(university_domain, f, ensure_ascii=False, indent=4)
